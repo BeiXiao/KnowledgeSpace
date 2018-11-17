c607edb104745c11e8ede7ac1f351ef2342d4ba9
马骏骁 <majx@rd.netease.com>
2018-11-17 20:53:55 +0800

    add space banner

--- src/components/space.vue
+++ src/components/space.vue
@@ -4,18 +4,47 @@
       text-xs-center
       wrap
     >
-      空间的简介
+    <!-- 顶部轮播图 -->
+     <v-carousel class="space-banner">
+        <v-carousel-item
+          v-for="(item,i) in bannerItems"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    <!-- 顶部轮播图 End -->
     </v-layout>
+
+    <H1>
+      title
+    </H1>
   </v-container>
 </template>
 
 <script>
   export default {
     data: () => ({
+      bannerItems: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+ 
     })
   }
 </script>
 
 <style>
-
+.space-banner {
+   height: 300px !important;
+}
 </style>
